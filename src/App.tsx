315ac437ba55c5d0fd183f17411794ea1ad25ac1
Leandro <leandroroncatto18@gmail.com>
2025-11-11 17:44:05 -0300

    testng mobile first chat page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import MobileChatsPage from "./pages/MobileChatsPage";
+
 function App() {
-  return (
-    <>
-      <div className="w-full h-screen bg-black"></div>
-    </>
-  );
+    return (
+        <>
+            <div className="w-full h-screen bg-gray-200/95">
+                <MobileChatsPage />
+            </div>
+        </>
+    );
 }
 
 export default App;
