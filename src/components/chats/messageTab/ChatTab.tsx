fc1f2ae70ed6fa9e982a936cffccb8ba7e03faf2
Leandro <leandroroncatto18@gmail.com>
2025-11-12 17:33:01 -0300

    feat: added Archived / Pinned / All chats sections

--- src/components/chats/messageTab/ChatTab.tsx
+++ src/components/chats/messageTab/ChatTab.tsx
@@ -1,10 +1,10 @@
-import { Search } from "lucide-react";
+import { Box, Search } from "lucide-react";
 import ChatItem from "./ChatItem";
 
 export default function ChatTab() {
     return (
         <>
-            <div className="w-full h-screen p-4 py-8 sm:pl-36 lg:max-w-[600px] sm:bg-white/30">
+            <div className="w-full h-screen p-4 py-8 sm:pl-36 lg:max-w-[600px] bg-black/8 overflow-y-scroll">
                 <div className="space-y-4">
                     <div>
                         <h1 className="text-3xl font-bold">Chats</h1>
@@ -18,8 +18,28 @@ export default function ChatTab() {
                         <Search className="absolute text-blue-500 top-3 left-3" size={20} />
                     </div>
                 </div>
+                <div className="pt-10 pb-3 border-b border-gray-300">
+                    <div className="flex items-center justify-start gap-4">
+                        <Box size={20} />
+                        <h2 className="text-sm font-semibold text-blue-500">Archived</h2>
+                    </div>
+                </div>
                 <div className="py-8">
-                    <div><ChatItem /></div>
+                    <h2 className="pb-4 text-base font-semibold text-gray-800">Pinned</h2>
+                    <ul className="flex flex-col gap-4">
+                        <ChatItem />
+                    </ul>
+                </div>
+                <div>
+                    <h2 className="pb-4 text-base font-semibold text-gray-800">All chats</h2>
+                    <ul className="flex flex-col gap-4">
+                        <ChatItem />
+                        <ChatItem />
+                        <ChatItem />
+                        <ChatItem />
+                        <ChatItem />
+                        <ChatItem />
+                    </ul>
                 </div>
             </div>
         </>
