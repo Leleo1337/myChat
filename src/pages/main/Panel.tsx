da6e85f84b85860b14ca61910f5238a7157eeb3e
Leandro <leandroroncatto18@gmail.com>
2025-11-14 16:37:52 -0300

    refactor: remove unused useEffect & activeChat

--- src/pages/main/Panel.tsx
+++ src/pages/main/Panel.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import NavigationBar from "../../components/ui/NavigationBar";
 import MessageContent from "./TabContent/MessageContent";
 import SettingsContent from "./TabContent/SettingsContent";
 
 export default function Panel() {
     const [activeTab, setActiveTab] = useState("message");
-    const activeChat = "dasd";
 
     const renderTabsContents = () => {
         switch (activeTab) {
@@ -19,10 +18,6 @@ export default function Panel() {
                 return <SettingsContent />;
         }
     };
-
-    useEffect(() => {
-        console.log(activeChat);
-    });
 
     return (
         <div className="relative flex flex-col w-full h-screen transition-colors duration-300 border-r border-gray-300 shadow-sm bg-gray-50 dark:bg-slate-900 dark:border-slate-800">
