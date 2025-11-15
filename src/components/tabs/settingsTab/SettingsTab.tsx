e3b2e9ad40c4c4b469ff9015c20a18662bc3f98f
Leandro <leandroroncatto18@gmail.com>
2025-11-14 23:27:12 -0300

    feat: add profile & account tabs

--- src/components/tabs/settingsTab/SettingsTab.tsx
+++ src/components/tabs/settingsTab/SettingsTab.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import SettingsSubContent from "./sub/SettingsSubContent";
 import AppearanceTab from "./sub/AppaeranceTab";
+import AccountTab from "./sub/AccountTab";
+import ProfileTab from "./sub/ProfileTab.tsx";
 
 export default function SettingsSection() {
     const [activeSection, setActiveSection] = useState("options");
@@ -10,6 +12,8 @@ export default function SettingsSection() {
             <div className="w-full h-full p-2 py-8 sm:pl-36 lg:max-w-[600px] bg-white border-r border-gray-300 dark:bg-slate-900 dark:border-slate-800 transition-colors duration-300">
                 {activeSection == "options" && <SettingsSubContent setActiveSection={setActiveSection} />}
                 {activeSection == "appearance" && <AppearanceTab setActiveSection={setActiveSection} />}
+                {activeSection == "profile" && <ProfileTab setActiveSection={setActiveSection} />}
+                {activeSection == "account" && <AccountTab setActiveSection={setActiveSection} />}
             </div>
         </>
     );
