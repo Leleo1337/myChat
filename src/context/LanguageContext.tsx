942c05d35688d4241ad29ce82cbd23326d36c06e
Leandro <leandroroncatto18@gmail.com>
2025-12-02 17:39:40 -0300

    refactor: remove changeActiveLanguage from language context

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -5,14 +5,12 @@ type languageContextTypes = {
     language: "portugues" | "english";
     setLanguage: React.Dispatch<React.SetStateAction<"portugues" | "english">>;
     languageData: typeof en;
-    changeActiveLanguage: () => void;
 };
 
 const LanguageContext = createContext<languageContextTypes>({
     language: "english",
     setLanguage: () => {},
     languageData: en,
-    changeActiveLanguage: () => {},
 });
 
 export default LanguageContext;
