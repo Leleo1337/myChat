797550ad816667ce131658d8ebfdada2c68060b7
Leandro <leandroroncatto18@gmail.com>
2025-12-02 17:19:56 -0300

    feat: add theme management states

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
 import FeaturesSection from "../../components/landingPage/FeaturesSection";
 import Footer from "../../components/landingPage/Footer";
-import LandingPageHeader from "../../components/landingPage/Header";
+import Header from "../../components/landingPage/Header";
 import HeroSection from "../../components/landingPage/HeroSection";
 import SecuritySection from "../../components/landingPage/SecuritySection";
+import { getTheme, setTheme } from "../../utils/toggleTheme";
 
 export default function LandingPage() {
+    const [currentTheme, setCurrentTheme] = useState(getTheme());
+
+    useEffect(() => {
+        if (currentTheme) setTheme(currentTheme);
+    }, [currentTheme]);
+
+    useEffect(() => {
+        const initialTheme = getTheme() || "light";
+        setCurrentTheme(initialTheme);
+    }, []);
+
     return (
         <>
-            <LandingPageHeader />
-            <HeroSection />
+            <Header setCurrentTheme={setCurrentTheme} currentTheme={currentTheme} />
+            <HeroSection currentTheme={currentTheme} />
             <FeaturesSection />
             <SecuritySection />
             <Footer />
