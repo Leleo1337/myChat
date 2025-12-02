7d14f71b02c78683ef46defa6802788c91db2c24
Leandro <leandroroncatto18@gmail.com>
2025-12-02 17:20:45 -0300

    styles: add dark theme styles

--- src/components/landingPage/FeatureItem.tsx
+++ src/components/landingPage/FeatureItem.tsx
@@ -7,11 +7,11 @@ export type featureItemTypes = {
 export default function FeatureItem({ icon, title, text }: featureItemTypes) {
     return (
         <>
-            <div className="p-8 bg-white border border-gray-300 shadow-sm rounded-2xl">
+            <div className="p-8 bg-white border border-gray-300 shadow-sm rounded-2xl dark:text-slate-100 dark:bg-slate-800 dark:border-slate-700 ">
                 <div className="flex flex-col gap-4">
                     <div className="p-3 w-fit bg-blue-500/30 rounded-xl">{icon}</div>
-                    <h1 className="text-xl font-semibold text-gray-800">{title} </h1>
-                    <p className="text-gray-600">{text}</p>
+                    <h1 className="text-xl font-semibold text-gray-800 dark:text-slate-100">{title} </h1>
+                    <p className="text-gray-600 dark:text-slate-300">{text}</p>
                 </div>
             </div>
         </>
