16f565d96d15f32ed108b0566f0352442140f018
Leandro <leandroroncatto18@gmail.com>
2025-12-08 13:41:36 -0300

    feat: add language context for translations

--- src/components/auth/ForgotPasswordForm.tsx
+++ src/components/auth/ForgotPasswordForm.tsx
@@ -1,21 +1,25 @@
 import { MailIcon } from "lucide-react";
 import InputIcon from "../ui/InputIcon";
+import { useContext } from "react";
+import LanguageContext from "../../context/LanguageContext";
 
 export default function ForgotPasswordForm() {
+    const { languageData } = useContext(LanguageContext);
+
     return (
         <>
             <div className="pb-10 md:pb-16">
                 <h1 className="pb-2 text-4xl font-extrabold text-center text-gray-900 lg:text-5xl dark:text-gray-50">
-                    Esqueceu sua senha?
+                    {languageData.auth?.forgotPasswordPage.title}
                 </h1>
                 <p className="text-center text-gray-600 dark:text-gray-400">
-                    Digite seu email para receber um link de recuperação.
+                    {languageData.auth?.forgotPasswordPage.subtitle}
                 </p>
 
                 <form onSubmit={(e) => e.preventDefault()} className="flex flex-col w-full gap-4 pt-12">
                     <div className="relative flex flex-col">
                         <label htmlFor="email" className="mb-1 text-sm text-gray-800 dark:text-gray-200">
-                            Email
+                            {languageData.auth?.forgotPasswordPage.emailLabel}
                         </label>
 
                         <InputIcon
@@ -28,7 +32,7 @@ export default function ForgotPasswordForm() {
                     </div>
 
                     <button className="p-4 font-semibold text-white bg-blue-500 cursor-pointer rounded-2xl hover:bg-blue-600">
-                        Enviar link de recuperação
+                        {languageData.auth?.forgotPasswordPage.submitButton}
                     </button>
                 </form>
             </div>
--- src/components/auth/ResetPasswordForm.tsx
+++ src/components/auth/ResetPasswordForm.tsx
@@ -1,16 +1,24 @@
 import { LockIcon } from "lucide-react";
 import InputIcon from "../ui/InputIcon";
+import { useContext } from "react";
+import LanguageContext from "../../context/LanguageContext";
 
 export default function ResetPasswordForm() {
+    const { languageData } = useContext(LanguageContext);
+    
     return (
         <>
             <div className="pb-10 md:pb-16">
-                <h1 className="text-4xl font-extrabold text-center text-gray-600 dark:text-gray-50">Redefinir senha</h1>
-                <p className="text-center text-gray-600 dark:text-gray-400">Escolha uma nova senha para sua conta</p>
+                <h1 className="text-4xl font-extrabold text-center text-gray-600 dark:text-gray-50">
+                    {languageData.auth?.forgotPasswordPage.resetTitle}
+                </h1>
+                <p className="text-center text-gray-600 dark:text-gray-400">
+                    {languageData.auth?.forgotPasswordPage.subtitle}
+                </p>
                 <form className="flex flex-col w-full gap-4 pt-10" onSubmit={(e) => e.preventDefault()}>
                     <div className="flex flex-col gap-1">
                         <label htmlFor="password" className="text-sm text-gray-800 dark:text-gray-200">
-                            New password
+                            {languageData.auth?.forgotPasswordPage.newPasswordLabel}
                         </label>
                         <InputIcon
                             Icon={LockIcon}
@@ -22,18 +30,18 @@ export default function ResetPasswordForm() {
                     </div>
                     <div className="flex flex-col gap-1">
                         <label htmlFor="confirm" className="text-sm text-gray-800 dark:text-gray-200">
-                            Confirm password
+                            {languageData.auth?.forgotPasswordPage.confirmPasswordLabel}
                         </label>
                         <InputIcon
                             Icon={LockIcon}
                             type="password"
-                            id="password"
+                            id="confirm"
                             name="confirm"
                             className="p-3 pl-10 text-gray-900 placeholder-gray-400 transition duration-150 bg-white border border-gray-300 rounded-lg shadow-sm dark:border-gray-600 dark:bg-slate-800 dark:text-gray-100 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
                     </div>
                     <button className="p-4 font-semibold text-white bg-blue-500 cursor-pointer rounded-2xl hover:bg-blue-600">
-                        Update password
+                        {languageData.auth?.forgotPasswordPage.updateButton}
                     </button>
                 </form>
             </div>
